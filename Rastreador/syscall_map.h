79ef6b918eb5a8e3bccc462eafd5732b4e810264
William Alfaro Quiros <williamyalfaro@gmail.com>
2025-03-28 19:07:57 -0600

    Creación de makefile y el header para la lista de syscalls

---
+++ Rastreador/syscall_map.h
@@ -0,0 +1,9 @@
+#ifndef SYSCALL_MAP_H
+#define SYSCALL_MAP_H
+
+// Arreglo que contiene los nombres de las syscalls para arquitectura x86_64.
+// Se accede mediante el número de syscall como índice.
+extern const char *syscall_names[];
+
+#endif // SYSCALL_MAP_H
+// Fin del archivo syscall_map.h
